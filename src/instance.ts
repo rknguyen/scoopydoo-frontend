66f33c5d24045c1b3e5df9eaee1cf716d0b00cfb
Tu Huynh <tu.huynh@linecorp.com>
2020-03-05 14:30:19 +0700

    change base url

--- src/instance.ts
+++ src/instance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:8081/api',
+  baseURL: '/api',
   timeout: 3000,
   headers: {
     Accept: 'application/json',
